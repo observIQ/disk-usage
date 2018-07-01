5e7875cc90dd630c292b773cc2e52c5597067a92
joe sirianni <joe.sirianni@bluemedora.com>
2018-07-01 15:23:47 -0400

    build for freebsd

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,3 @@
+env GOOS=freebsd GOARCH=amd64 go build -v -o disk-usage-bsd.bin
 env GOOS=windows GOARCH=amd64 go build -v -o disk-usage-win.exe
 env GOOS=linux GOARCH=amd64 go build -v -o disk-usage.bin
