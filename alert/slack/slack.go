728ab741410759d9c625618900e904cde16ddba1
jsirianni <joe.sirianni@bluemedora.com>
2021-02-11 21:06:40 -0500

    fix tests, add more tests

--- alert/slack/slack.go
+++ alert/slack/slack.go
@@ -26,9 +26,23 @@ func (a Slack) Init() error {
         return fmt.Errorf("slack webhook url is not set")
     }
 
-    if _, err := url.Parse(a.HookURL); err != nil {
+    u, err := url.Parse(a.HookURL)
+    if err != nil {
         return errors.Wrap(err, "slack webhook url failed to parse")
     }
 
+    if err := validateScheme(u.Scheme); err != nil {
+        return err
+    }
+
     return nil
+}
+
+func validateScheme(s string) error {
+    for _, scheme := range []string{"http","https"} {
+        if s == scheme {
+            return nil
+        }
+    }
+    return fmt.Errorf(fmt.Sprintf("slack webhook url should use scheme 'http' or 'https', got %s", s))
 }
--- alert/slack/slack_test.go
+++ alert/slack/slack_test.go
@@ -2,15 +2,57 @@ package slack
 
 import (
     "testing"
+
+    "github.com/stretchr/testify/require"
 )
 
 func TestSend(t *testing.T) {
-    a := Alert{
-        Message: "fake",
+    a := Slack{
         Channel: "fake",
-        URL: "https://fake33slack11url.com",
+        HookURL:"https://fake33slack11url.com",
     }
-    if err := a.Send(); err == nil {
-        t.Errorf("Expected an error when POSTing to " + a.URL)
+    if err := a.Send("some message"); err == nil {
+        t.Errorf("Expected an error when POSTing to " + a.HookURL)
+    }
+}
+
+func TestInit(t *testing.T) {
+    cases := []struct{
+        name     string
+        config   Slack
+        expectedErr bool
+    }{
+        {
+            name: "channel is not required",
+            config: Slack{
+                HookURL: "http://validurl.com",
+            },
+            expectedErr: false,
+        },
+        {
+            name: "scheme is missing",
+            config: Slack{
+                HookURL: "validurl.com",
+            },
+            expectedErr: true,
+        },
+        {
+            name: "invalid scheme",
+            config: Slack{
+                HookURL: "ftp://validurl.com",
+            },
+            expectedErr: true,
+        },
+    }
+
+    for _, tc := range cases {
+        t.Run(tc.name, func(t *testing.T) {
+            err := tc.config.Init()
+            if tc.expectedErr {
+                require.Error(t, err)
+            } else {
+                require.NoError(t, err)
+            }
+        })
     }
 }
