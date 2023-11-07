6250f34bef41f17b91b456b031c758bf84cedd25
Joseph Sirianni <joe.sirianni@observiq.com>
2023-11-07 11:43:06 -0500

    Setup actions (#31)
    * Remove needs
    
    * fix script
    
    * remove linter

--- scripts/test/generic_unix.sh
+++ scripts/test/generic_unix.sh
@@ -2,16 +2,16 @@
 
 ./disk-usage -t 1
 OUT=$(jq .alerted /tmp/suppress | jq length)
-if [[ "$OUT" -eq 0 ]]; then
+if [ "$OUT" -eq 0 ]; then
   echo "expected state to not be empty"
   exit 1
 fi
 
 ./disk-usage -t 99
 OUT=$(jq .alerted /tmp/suppress | jq length)
-if [[ ! "$OUT" -eq 0 ]]; then
+if [ "$OUT" -eq 0 ]; then
     echo "expected state to be empty"
-    exit 1
+  exit 1
 fi
 
 exit 0
